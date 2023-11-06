7cd05faedf1cf5ac3fafb5d8fc482e2e66e65826
Eneas Baroni <eneasbaroni@gmail.com>
2023-11-05 22:56:33 -0300

    init

---
+++ src/data/data.js
@@ -0,0 +1,39 @@
+const data = [
+    {
+        nombre : 'Dromo',
+        tareas: [
+            'Social media',
+            'Content creator'
+        ],
+        img : '/images/works/dromo.jpg',
+        link: 'https://www.instagram.com/dromo.oficial/'
+    },
+    {
+        nombre : 'Utzzia',
+        tareas: [
+            'Social media',
+            'Content creator'
+        ],
+        img : '/images/works/utzzia.jpg',
+        link: 'https://www.instagram.com/utzzia'
+    },
+    {
+        nombre : 'Tiendas Genes',
+        tareas: [
+            'Social media',
+            'Content creator'
+        ],
+        img : '/images/works/genes.jpg',
+        link: 'https://www.instagram.com/tiendas.genes'
+    },
+    {
+        nombre : 'Martinez Rubiano',
+        tareas: [
+            'Copywriter'
+        ],
+        img : '/images/works/martinez.webp',
+        link: 'https://martinezrubiano.com.ar/'
+    }
+]
+
+export default data
